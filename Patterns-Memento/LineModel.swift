dcdeabe0404078ba33382ca188d5fbd0d6c6f8bb
Rus <de.mr44@gmail.com>
2022-01-24 23:20:48 +0500

    Added LineModel

---
+++ Patterns-Memento/LineModel.swift
@@ -0,0 +1,14 @@
+//
+//  LineModel.swift
+//  Patterns-Memento
+//
+//  Created by Ruslan on 24.01.2022.
+//
+
+import UIKit
+
+struct LineModel {
+    var start: CGPoint
+    var end: CGPoint
+    var color: UIColor
+}
